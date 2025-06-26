65e990488a60aed0e7743e47dc7cb0e8963823a8
mikmak29 <mjogamaapple@gmail.com>
2025-06-26 15:07:15 +0800

    Updated Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        allura: ['Allura', 'cursive'],
+        anton: ['Anton', 'sans-serif'],
+        cormorant: ['Cormorant Garamond', 'serif'],
+        ebgaramond: ['EB Garamond', 'serif'],
+        greatvibes: ['Great Vibes', 'cursive'],
+        inter: ['Inter', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        parisienne: ['Parisienne', 'cursive'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      screens: {
+        'xs': '430px',
+        'ss': '540px',
+        'laptop': '1024px',
+        'ipadpro': { 'raw': '(width: 1024px) and (min-height: 1000px)' },
+        'nesthub': { 'raw': '(width: 1024px) and (max-height: 700px)' },
+        'nesthubmax': { 'raw': '(width: 1280px) and (max-height: 900px)' },
+      },
+    },
   },
   plugins: [],
 }
